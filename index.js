fdabf358bdc7b28a30be8f30d41874acde609184
Julieth Kruse <yuli2546@hotmail.com>
2024-05-21 11:25:01 -0500

    added README file with more details about database

--- index.js
+++ index.js
@@ -54,78 +54,11 @@ var attires = [
     },
 ];
     
-    
-// ---------option 3
-// var attires = async () => {
-//      attires = await getClothing();
-//     //  setTimeout(attires,15000);
-// return (attires);
-// }
-
-// async function getClothing(){
-// 	var url = 'https://fakestoreapi.com/products';	
-// 	var response = await fetch(url);
-// 	var json     = await response.json();
-//     console.log(json.data);
-// 	return json.data;
-// }
-
-// setTimeout(getClothing,15000);
-// console.log(JSON.stringify(getClothing));
-
-// ---------option 3 correction? text instead json
-// async function getClothing(){
-//     let response = await fetch('https://fakestoreapi.com/products');
-//     let data = await response.text();
-//     console.log(data);
-// }
-
-
-// ---------option 6
-// async function getClothing() {
-//     const response = await fetch("https://fakestoreapi.com/products");
-//     const attires = await response.json();
-//     // console.log(attires);
-//   }
-  
-//   var attires = getClothing();
-//   console.log(attires);
-
-// ---------option 1
-// var attires = [{id:0,name:"sleeves"},{id:1,name:"short"}]
-
-// ---------option 2
-// var attires = async () => {
-//     const response = await fetch("https://fakestoreapi.com/products");
-//     const clothesData = await response.json();
-//     console.log(clothesData);
-//     return clothesData;
-//   };
-  
-// ---------option 4 
-// const requestOptions = {
-//     method: "GET",
-//     redirect: "follow"
-//   };
-  
-//   fetch("https://fakestoreapi.com/products", requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => console.log(result))
-//     .catch((error) => console.error(error));
-
-// ---------option 5
-// var attires = fetch("https://fakestoreapi.com/products")
-//   .then((res) => res.json())
-//   .then((json) => console.log(json))
-
 
 const app = express();
 app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
-
-
 app.get("/attires", (req,res)=>{
     res.send(attires);
 })
@@ -143,22 +76,11 @@ app.put("/attiresUpdate/:id", (req,res)=>{
     if(!attires[req.params.id]) {
         throw new Error("Attire ID doesnt exist " + req.body.params)
       }
-    //   attires[req.body.id] = {
-    //     ...attires[req.body.id],
-    //     ...attires,
-    //   };
-    //      res.send("attires updated:"+JSON.stringify(attires));
-    //   return attires[req.body.id];
 
       attires[req.params.id] = ({id: req.body.id, title:req.body.title, price: req.body.price}),
-        
-     
       console.log(JSON.stringify(attires[req.body.id]))
       res.send("attires updated:"+JSON.stringify(attires));
       return attires[req.body.id];
-
-    
-    
 })
 
 app.listen(4000,()=>console.log('Listening on 4000'))
